5b9c40c3e2bf3f68874e222f50d9bc0d135c8d08
seyedali <seyed.ali.devl@gmail.com>
2025-09-13 07:12:22 +0330

    feat(env): use frontend environment variable

--- frontend/src/constants/EnvironmentConstants.ts
+++ frontend/src/constants/EnvironmentConstants.ts
@@ -1,7 +1,5 @@
-import {RunningMode} from "@/constants/RunningMode";
-
 export const Environment = {
-    NODE_ENV: RunningMode.DEVELOPMENT, // "production" or "development"
-    API_URL: "https://api.freetoolsy.com", // backend proxied under /api by caddy
+    NODE_ENV: process.env.NODE_ENV, // "production" or "development"
+    API_URL: "https://pm.freetoolsy.com",
     API_URL_DEV: "http://localhost:8086"
 };
