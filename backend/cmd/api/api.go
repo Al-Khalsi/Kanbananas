56f0d71985bd681514560fbfdf5de0b1f11b04c1
seyedali <seyed.ali.devl@gmail.com>
2025-09-14 06:18:56 +0330

    chore(route): modify backend route

--- backend/cmd/api/api.go
+++ backend/cmd/api/api.go
@@ -17,7 +17,7 @@ func RegisterRoutes(db *gorm.DB) *gin.Engine {
 	router.Use(corsMiddleware)
 	router.Use(RegisterMiddlewares())
 
-	baseUrlApi := router.Group("/api/pm")
+	baseUrlApi := router.Group("/pm/api")
 	registerColumnHandler(db, baseUrlApi)
 	registerTaskHandler(db, baseUrlApi)
 
